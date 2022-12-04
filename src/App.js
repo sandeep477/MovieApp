4ef7253f257f7b1f0f25afeed0541001f99d1d72
sandeep upadhyay <https://github.com/sandeep477>
2022-12-04 23:56:32 +0530

    Routing done first part

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Banner from './Components/Banner';
 import Movies from './Components/Movies';
+import Favourite from './Components/Favourite';
+import {BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
+    <Router>
     <Navbar/>
-    <Banner/>
-    <Movies/>
-    </div>
+    <Routes>
+      <Route path='/'  element={<Movies/>}/>
+      <Route path='/favourite' element={<Favourite/>}/>
+    </Routes>
+    </Router>
   );
 }
 
